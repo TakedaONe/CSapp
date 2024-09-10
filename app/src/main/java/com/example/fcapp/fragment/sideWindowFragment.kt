f550bd334cae063acd968a41e108956267302f12
TakedaONe <anuaraidar15@gmail.com>
2024-09-10 23:03:30 +0600

    1 master

---
+++ app/src/main/java/com/example/fcapp/fragment/sideWindowFragment.kt
@@ -0,0 +1,19 @@
+package com.example.fcapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fcapp.R
+
+class sideWindowFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_side_window, container, false)
+    }
+}
